edccba92cac2784ae7cce30d7f286418754e1a06
Nate Cheng <qqnc@users.noreply.github.com>
2015-12-03 23:50:48 -0800

    Update Solution.rb

--- Contains Duplicate III/Solution.rb
+++ Contains Duplicate III/Solution.rb
@@ -8,9 +8,6 @@ numbers = Array.[](2,3,4,2,3,5,6,8,9,3,4,7,6,4,5,8,9,0)
 def contains_nearby_duplicate(nums, k, t)
   min = 4611686018427387903
   diff=0;
-  puts "---------------"
-  puts nums.size
-  puts "---------------"
   hash = Hash.new
   nums[0..-1].each_with_index { |val,i|
     if hash.has_key?(val)
