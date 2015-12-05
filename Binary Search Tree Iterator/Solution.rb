15105c86499930ab819c42394b7a08020386c166
Nate <boo0330@gmail.com>
2015-12-05 21:59:45 +0000

    BST iterator

---
+++ Binary Search Tree Iterator/Solution.rb
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val)
+#         @val = val
+#         @left, @right = nil, nil
+#     end
+# end
+
+class BSTIterator
+    # @param {TreeNode} root
+    def initialize(root)
+        
+    end
+
+    # @return {Boolean}
+    def has_next
+        
+    end
+
+    # @return {Integer}
+    def next
+        
+    end
+end
+
+# Your BSTIterator will be called like this:
+# i, v = BSTIterator.new(root), []
+# while i.has_next()
+#    v << i.next
+# end
